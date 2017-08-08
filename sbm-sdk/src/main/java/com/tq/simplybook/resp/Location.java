d01c8f2cceb2d08418a0622bb43f68aed7c89003
lccanh <lccanh@tma.com.vn>
2017-08-08 18:00:58 +0700

    Fix issue with parsing Location

---
+++ sbm-sdk/src/main/java/com/tq/simplybook/resp/Location.java
@@ -0,0 +1,25 @@
+package com.tq.simplybook.resp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Location {
+    
+    private String id = null;
+    private String title = null;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
