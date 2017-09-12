7a882785c130e9ac2c45370ab2ec1b83caa25795
lccanh <lccanh@tma.com.vn>
2017-09-12 11:44:23 +0700

    fix import failure

--- sbm-sdk/src/main/java/com/tq/simplybook/service/SpecialdayServiceSbm.java
+++ sbm-sdk/src/main/java/com/tq/simplybook/service/SpecialdayServiceSbm.java
@@ -2,8 +2,6 @@ package com.tq.simplybook.service;
 
 import com.tq.simplybook.exception.SbmSDKException;
 import com.tq.simplybook.req.DayInfoReq;
-import com.tq.simplybook.req.SpecialDayReq;
-import com.tq.simplybook.test.WorkdayInfo;
 
 public interface SpecialdayServiceSbm {
 	public boolean blockTimeSlot(String companyLogin, String endpoint, String token, DayInfoReq info)
