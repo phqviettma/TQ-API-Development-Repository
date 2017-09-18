3dd8f2266acb6e51ee403d3b589b35f827bacfc4
lccanh <lccanh@tma.com.vn>
2017-09-18 18:57:05 +0700

    add UTC time util

---
+++ cliniko-sdk/src/main/java/com/tq/cliniko/time/UtcTimeUtil.java
@@ -0,0 +1,18 @@
+package com.tq.cliniko.time;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class UtcTimeUtil {
+	public static String getNowInUTC(String timezone) {
+		String interim = ZonedDateTime.now(ZoneId.of(timezone)).truncatedTo(ChronoUnit.SECONDS)
+							.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+		return interim + "Z";
+	}
+	
+	public static String toUtcToBasicFormat(String utcDatetime) {
+		return utcDatetime.replace("T", " ").replace("Z", "");
+	}
+}
