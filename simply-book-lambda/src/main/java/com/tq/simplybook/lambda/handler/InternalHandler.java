fdefd175a18165972e9edff1634981ae09b503f2
TMA\pttsuong <pttsuong@tma.com.vn>
2018-02-26 18:13:13 +0700

    remove unuse lib

--- simply-book-lambda/src/main/java/com/tq/simplybook/lambda/handler/InternalHandler.java
+++ simply-book-lambda/src/main/java/com/tq/simplybook/lambda/handler/InternalHandler.java
@@ -4,7 +4,6 @@ import com.tq.calendar.exception.GoogleApiSDKException;
 import com.tq.cliniko.exception.ClinikoSDKExeption;
 import com.tq.simplybook.exception.SbmSDKException;
 import com.tq.simplybook.lambda.model.PayloadCallback;
-import com.tq.simplybook.resp.BookingInfo;
 
 public interface InternalHandler {
     void handle(PayloadCallback payload) throws SbmSDKException, ClinikoSDKExeption, GoogleApiSDKException;
